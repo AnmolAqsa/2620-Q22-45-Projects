f5a8505c807af512f336d2af65425e9c8522037b
AnmolAqsa <165940283+AnmolAqsa@users.noreply.github.com>
2024-07-04 12:46:43 -0700

    Add files via upload
    Q22, 45 Projects

---
+++ main.js
@@ -0,0 +1,9 @@
+"use strict";
+let frinds = ["Anmol", "Aqsa", "Sajida", "Ayaan", "Arman"];
+frinds.slice();
+for (let i = 0; i < frinds.length; i++) {
+    console.log(frinds[8]);
+}
+for (let i = 0; i < frinds.length; i++) {
+    console.log(frinds[i]);
+}
---
+++ main.ts
@@ -0,0 +1,15 @@
+/*
+Q22. Intentional Error: If you haven’t received an array index error in one of your programs yet, 
+try to make one happen. Change an index in one of your programs
+to produce an index error. Make sure you correct the error before closing the program.
+*/
+
+let frinds: string[] = ["Anmol","Aqsa","Sajida","Ayaan","Arman"];
+frinds.slice()
+
+for (let i=0;i<frinds.length;i++){
+    console.log(frinds[8]);
+}
+for (let i=0;i<frinds.length;i++){
+    console.log(frinds[i])
+}
